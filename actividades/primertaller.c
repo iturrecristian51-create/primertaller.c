abaab793b72da01a422fa05d2eb8714a6197dfb0
iturrecristian51-create <iturrecristian51@gmail.com>
2025-09-07 15:35:16 -0500

    menu de interacion
    menu con todo lo que pide el taller

--- actividades/primertaller.c
+++ actividades/primertaller.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 
-int opcion;
-do{
-    printf("menu");
+int main(void) {
+ int opcion;
+    do {
+        printf("\n  menu\n");
+        printf("1.llenar lista\n");
+        printf("2.mostrarlista\n");
+        printf("3.ordenar de forma acendente\n");
+        printf("4.ordenar de forma decendente\n");
+        printf("5.salir\n");
+        printf("elige una opcion\n");
+        
+    scanf("%d", &opcion);
+    
+       switch (opcion)
+       {
+       case 1:
+        printf("llenando  lista\n");
+        break;
+
+        case 2:
+        printf("mostrando lista\n");
+        break;
+
+        case 3:
+        printf("ordenando acendente\n");
+        break;
+       
+        case 4:
+        printf("ordenando decendente\n");
+        break;
+
+        case 5:
+        printf("saliendo delprograma\n");
+        break;
+
+
+       default:
+        break;
+
+       } 
+        
+        
+        
+        
+
+        
+        opcion = 0; 
+    } while (opcion != 5); 
+
+    return 0;
 }
