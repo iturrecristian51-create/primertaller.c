a00435c48e4b7d74f47ccefedf4d8e1912e9b980
iturrecristian51-create <iturrecristian51@gmail.com>
2025-09-07 19:27:34 -0500

    ordenado de forma decendente finalisado
    este me costo profe

--- actividades/primertaller.c
+++ actividades/primertaller.c
@@ -23,11 +23,35 @@ void llenararray(int a[], int n){
 
 }
 
+
+
 void mostrararray(int a[],int n){
     for (int i = 0; i < n; i++){
     printf("%d\n", a[i]);
     }
 printf("\n");
+}
+
+void ordenaracendente(int a[], int n){
+
+    for (int i = 0; i < n-1; i++){
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (a[j]>a[j+1]){
+            int temp=a[j];
+            a[j]= a[j+1];
+            a[j+1]=temp;
+            
+                
+            
+            }
+        }
+        
+    }
+    
+     printf("Lista ordenada de forma ascendente.\n");   
+    
+    
 }
 
 
@@ -61,6 +85,7 @@ int main(void) {
 
         case 3:
         printf("ordenando acendente\n");
+        ordenaracendente(arreglo,10);
         break;
        
         case 4:
